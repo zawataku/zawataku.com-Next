4f7641760acb1f9b8f9ee260645e0008ba61260f
zawataku <c1254976@st.kanazawa-it.ac.jp>
2024-11-11 14:20:42 +0900

    update:404ページ

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,17 +1,75 @@
-import React from "react";
-import Link from "next/link";
+"use client"
+import styled from 'styled-components';
+
+const Not_found_img = styled.img`
+    width: 100%;
+`
+
+const Not_found_Body_Top = styled.div`
+    max-width: 42rem;
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 4rem;
+    margin-bottom: 4rem;
+    padding-left: 2rem;
+    padding-right: 2rem;
+    background-color: white;
+`;
+
+const Not_Found_Top_Section = styled.section`
+    margin-bottom: 3rem;
+    text-align: center;
+    align-items: center;
+`;
+
+const Not_found_Copyright = styled.p`
+    font-size: 1rem;
+    @media screen and (max-width: 425px){
+        font-size: 0.875rem;   
+    }
+`;
+
+const Not_found_Title = styled.h1`
+    margin-top: 1rem;
+    margin-bottom: 1rem;
+    font-size: 4rem;
+    font-weight: bold;
+    @media screen and (max-width: 425px){
+        font-size: 3rem;   
+    }
+`
+
+const Not_found_Text = styled.p`
+    font-size: 1rem;
+    margin-top: 1rem;
+    margin-bottom: 2rem;
+    @media screen and (max-width: 425px){
+        font-size: 0.875rem;   
+    }
+`;
+
+const Not_found_Link = styled.a`
+    font-size: 1rem;
+    margin-top: 1rem;
+    margin-bottom: 2rem;
+    font-weight: bold;
+    color: #0000ee;
+
+    &.visited{
+        color: #0000ee;
+    }
+`;
 
 export default function Custom404() {
     return (
-        <div className="h-screen bg-white p-5 text-center">
-            <img src="/img/404/404.webp" alt="404_image" className="mx-auto hidden h-4/6 md:block lg:block" />
-            <img src="/img/404/404.webp" alt="404_image" className="mx-auto md:hidden lg:hidden" />
-            <p className="text-sm text-slate-900">Illustration by 2個 (@chohjaharaSA)</p>
-            <h1 className="hidden py-4 text-5xl font-bold text-slate-900 md:block lg:block">ページが見つかりませんでした...</h1>
-            <h1 className="block py-5 text-3xl font-bold text-slate-900 md:hidden lg:hidden">ページが<br />見つかりませんでした...</h1>
-            <p className="text-base text-slate-900">お探しのページは一時的にアクセスができない状況にあるか，移動もしくは削除された可能性があります．</p>
-            <p className="my-5"><Link href="/" className="leading-10 text-sky-600 hover:underline">トップに戻る</Link></p>
-        </div>
-
+        <Not_found_Body_Top>
+            <Not_Found_Top_Section>
+                <Not_found_img src="/img/404.webp" alt="ページが見つかりませんでした" />
+                <Not_found_Copyright>Illustration by 2個 (@chohjaharaSA)</Not_found_Copyright>
+                <Not_found_Title>404 Not Found</Not_found_Title>
+                <Not_found_Text>お探しのページは一時的にアクセスができない状況にあるか，移動もしくは削除された可能性があります．</Not_found_Text>
+                <Not_found_Link href='/'>トップに戻る</Not_found_Link>
+            </Not_Found_Top_Section>
+        </Not_found_Body_Top>
     );
 }
